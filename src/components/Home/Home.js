45dbe6aec10993f0cfc9350b0061799c8b39a227
joshmg88 <joshmg88@gmail.com>
2018-09-12 16:25:51 -0500

    adjusted borders

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,58 +1,67 @@
 import React, { Component } from 'react';
 
-import axios from 'axios'
-import { updateRole } from '../../ducks/userReducer'
-import { connect } from 'react-redux'
+import axios from 'axios';
+import { updateRole } from '../../ducks/userReducer';
+import { connect } from 'react-redux';
 
-import Quote from '../../components/Quote/Quote'
-import './Home.css'
+import Quote from '../../components/Quote/Quote';
+import './Home.css';
 
 class Home extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super();
+    this.state = {
+      role: '',
+      id: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-        this.state = {
-            role: '',
-            id: 0
-        };
-        this.handleClick = this.handleClick.bind(this);
+  componentDidMount() {
+    axios.get('/api/me').then(res => {
+      this.setState({
+        id: res.data.id
+      });
+    });
+  }
+  async handleClick(val) {
+    let { id } = this.state;
+    await this.setState({
+      role: val
+    });
+    await this.props.updateRole(this.state.role, id);
+  }
+  render() {
+    if (this.props.user.didErr || this.props.user.user.role) {
+      return (
+        <div>
+          <Quote />
+        </div>
+      );
+    } else {
+      return (
+        <div className="home">
+          <p>
+            Thank you for sigining up, please let us know if you are an employee
+            or employer.
+          </p>
+          <button className="btn" onClick={() => this.handleClick('employer')}>
+            Sign up as Employer
+          </button>
+          <button className="btn" onClick={() => this.handleClick('employee')}>
+            Sign up as Employee
+          </button>
+          <Quote />
+        </div>
+      );
     }
-
-    componentDidMount() {
-        axios.get('/api/me').then(res => {
-            this.setState({
-                id: res.data.id
-            });
-        })
-    }
-    async handleClick(val) {
-        let { id } = this.state
-        await this.setState({
-            role: val
-        })
-        await this.props.updateRole(this.state.role, id)
-    }
-    render() {
-        if (this.props.user.didErr || this.props.user.user.role) {
-            return (
-                <div>
-                    <Quote />
-                </div>
-            )
-        } else {
-            return (
-                <div className='home'>
-                    <button className="btn" onClick={() => this.handleClick('employer')}>Sign up as Employer</button>
-                    <button className="btn" onClick={() => this.handleClick('employee')}>Sign up as Employee</button>
-                    <Quote />
-                </div>
-            )
-        }
-    }
+  }
 }
-
 
 let mapStateToProps = state => state;
 
-export default connect(mapStateToProps, { updateRole })(Home);
+export default connect(
+  mapStateToProps,
+  { updateRole }
+)(Home);
--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,75 +1,80 @@
 import React, { Component } from 'react';
-import { getJobs, deleteJob, selectEmployee } from '../../ducks/jobReducer'
-import { getEmployees } from '../../ducks/employeeReducer'
-import { connect } from 'react-redux'
-import './Jobs.css'
+import { getJobs, deleteJob, selectEmployee } from '../../ducks/jobReducer';
+import { getEmployees } from '../../ducks/employeeReducer';
+import { connect } from 'react-redux';
+import './Jobs.css';
 
 class Jobs extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super()
+    this.state = {};
+    this.handleDelete = this.handleDelete.bind(this);
+  }
 
-        this.state = {
+  componentDidMount() {
+    this.props.getJobs();
+    this.props.getEmployees();
+  }
 
-        };
-        this.handleDelete = this.handleDelete.bind(this);
-    }
+  async handleDelete(e) {
+    await this.props.deleteJob(e);
 
-    componentDidMount() {
-        this.props.getJobs()
-        this.props.getEmployees()
-    }
+    await this.props.getJobs();
+  }
 
-    async handleDelete(e) {
-        await this.props.deleteJob(e)
+  render() {
+    const { jobs } = this.props.job;
+    const { employees } = this.props.employee;
 
-        await this.props.getJobs()
-    }
+    let jobsList = jobs.map((papaE, papaI) => {
+      return (
+        <div className="customerList" key={papaI}>
+          <h3 className="customerTitle">Customer Name:</h3>
+          <h4 className="customerName">{papaE.name}</h4> <br />
+          <h4 className="customerTitle">Address:</h4>
+          {papaE.address} <br />
+          <h4 className="customerTitle">Job Details:</h4>
+          {papaE.details} <br />
+          <h4 className="customerTitle">Price:</h4>${papaE.price} <br />
+          <h4 className="customerTitle">Est Hours:</h4>
+          {papaE.est_hours} <br />
+          <button className="btn" onClick={() => this.handleDelete(papaE)}>
+            Delete
+          </button>
+          <select
+            onChange={e =>
+              this.props.selectEmployee(e.target.value, papaE.job_id)
+            }
+          >
+            <option value="0" selected disabled>
+              Employees:
+            </option>{' '}
+            {employees.map((e, i) => {
+              return (
+                <option key={i} value={e.id}>
+                  {e.name}
+                </option>
+              );
+            })}
+          </select>{' '}
+          <br />
+        </div>
+      );
+    });
 
-    render() {
-        const { jobs } = this.props.job
-        const { employees } = this.props.employee
-
-        let jobsList = jobs.map((papaE, papaI) => {
-
-            return <div className='customerList' key={papaI}>
-                <h3 className='customerTitle'>Customer Name:</h3>
-                <h4>{papaE.name}</h4> <br />
-                <h4 className='customerTitle'>Address:</h4>
-                {papaE.address} <br />
-                <h4 className='customerTitle'>Job Details:</h4>
-                {papaE.details} <br />
-                <h4 className='customerTitle'>Price:</h4>
-                {papaE.price} <br />
-                <h4 className='customerTitle'>Est Hours:</h4>
-                {papaE.est_hours} <br />
-                <button className="btn" onClick={() => this.handleDelete(papaE)}>Delete</button>
-
-
-                <select onChange={(e) => this.props.selectEmployee(e.target.value, papaE.job_id)}> {
-                    employees.map((e, i) => {
-                        return <option key={i} value={e.id}>
-
-                            {e.name}
-
-                        </option>
-
-
-                    })}</select> <br />
-            </div>
-        })
-
-        return (
-            <div>
-                <h2>Current Jobs</h2>
-                <div className="allJobs">
-                    {jobsList}
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h2>Current Jobs</h2>
+        <div className="allJobs">{jobsList}</div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => state
+const mapStateToProps = state => state;
 
-export default connect(mapStateToProps, { getJobs, deleteJob, getEmployees, selectEmployee })(Jobs);
+export default connect(
+  mapStateToProps,
+  { getJobs, deleteJob, getEmployees, selectEmployee }
+)(Jobs);
